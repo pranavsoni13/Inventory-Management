602a05ec0a5d251a53561c929ba581e79d8b0671
Pranav Soni <sonipranav2004@gmail.com.com>
2026-04-22 11:36:40 +0530

    updated api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://inventory-management-production-2a9a.up.railway.app",
 });
 
 // 🔐 token auto attach
